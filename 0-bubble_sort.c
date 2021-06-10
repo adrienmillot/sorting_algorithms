64f41b77dd7fe72795b760847202af98abdd15b2
SimonBr017 <simonbrard017@gmail.com>
2021-06-10 15:01:46 +0200

    add task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,19 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t cloop, cloop2;
+	int tmp;
+
+	for (cloop2 = 1; cloop2 <= size; cloop2++)
+	{
+		for (cloop = 0; cloop < size - 1; cloop++)
+			if (array[cloop] > array[cloop + 1])
+			{
+				tmp = array[cloop];
+				array[cloop] = array[cloop + 1];
+				array[cloop + 1] = tmp;
+				print_array(array, size);
+			}
+	}
+}
