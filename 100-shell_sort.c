017d118752bddb6444bab0d2c1860fce23d399ba
SimonBr017 <simonbrard017@gmail.com>
2021-06-12 21:35:47 +0200

    fix shell_sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,29 +2,38 @@
 
 void shell_sort(int *array, size_t size)
 {
-	size_t interval, cLoop1, cLoop2;
+	size_t cLoop1, cLoop2, tmp, interval = 0;
 	int valueToInsert;
 
-	while (interval < size / 3)
-		interval = interval * 3 + 1;
+	if (array == NULL || size < 2)
+		return;
 
+	while (interval <= size / 3)
+	{
+		interval = interval * 3 + 1;
+		
+	}
+	
 	while (interval > 0)
 	{
+		
 		for (cLoop1 = interval; cLoop1 < size; cLoop1++)
 		{
 			valueToInsert = array[cLoop1];
+		
 			cLoop2 = cLoop1;
-
+			
 			while (cLoop2 > interval - 1 && array[cLoop2 - interval] >= valueToInsert)
 			{
-				array[cLoop2] = array[cLoop2 - interval];
-				cLoop2 -= interval;
+				tmp = array[cLoop2 - interval];
+				array[cLoop2 - interval] = array[cLoop2];
+				array[cLoop2] = tmp;
+				cLoop2 -= interval;			
 			}
-
-			array[cLoop2] = valueToInsert;
+				
 		}
-
 		interval = (interval - 1) / 3;
 		print_array(array, size);
 	}
+	
 }
