28294f7bbf94a91e387adcbc62cca64448968168
Adrien MILLOT <adrien.millot@gmail.com>
2021-06-15 10:38:25 +0200

    Fix functions documentation

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,11 +1,17 @@
 #include "sort.h"
 
+/**
+ * swap_node_after - swap current node with next one
+ * @prmCurrent: current node
+ * Return: nothing void
+ */
+
 void swap_node_after(listint_t **prmCurrent)
 {
 	listint_t *previous, *after;
 
 	previous = (*prmCurrent)->prev;
-	after    = (*prmCurrent)->next;
+	after = (*prmCurrent)->next;
 
 	if (previous != NULL)
 		previous->next = after;
@@ -17,12 +23,18 @@ void swap_node_after(listint_t **prmCurrent)
 		(*prmCurrent)->next->prev = *prmCurrent;
 }
 
+/**
+ * swap_node_before - swap current node with previous one
+ * @prmCurrent: current node
+ * Return: nothing void
+ */
+
 void swap_node_before(listint_t **prmCurrent)
 {
 	listint_t *previous, *after;
 
 	previous = (*prmCurrent)->prev;
-	after    = (*prmCurrent)->next;
+	after = (*prmCurrent)->next;
 
 	(*prmCurrent)->prev = previous->prev;
 	(*prmCurrent)->next = previous;
@@ -34,13 +46,19 @@ void swap_node_before(listint_t **prmCurrent)
 		after->prev = previous;
 }
 
+/**
+ * cocktail_sort_list - function that sorts a doubly linked list of
+ * integers in ascending order using the Cocktail shaker sort algorithm
+ * @list: pointer to list to sort
+ * Return: nothing void
+ */
+
 void cocktail_sort_list(listint_t **list)
 {
 	int swapped;
 	listint_t *head = *list;
 
-	do
-	{
+	do {
 		swapped = 0;
 
 		while (head->next != NULL)
