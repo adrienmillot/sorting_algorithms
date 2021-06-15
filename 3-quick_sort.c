8c851410ed1fe1a22afc533ba162f9df171281e3
SimonBr017 <simonbrard017@gmail.com>
2021-06-15 11:03:57 +0200

    test low-1

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -66,12 +66,12 @@ void swap(int *a, int *b)
 int partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = low - 1;
+	int i = low;
 	int j;
 
-	for (j = low; j <= high; j++)
+	for (j = low; j < high; j++)
 	{
-		if (array[j] <= pivot)
+		if (array[j] < pivot)
 		{
 			if (i != j)
 			{
